7eb973196cd85ffa27e56069bb642ffda837466b
Christian Jensen <christian@orbik.com>
2021-03-30 23:51:22 -0700

    Add in a utility function to clear out emails
    Having this utility function is useful so the list of sent emails can be cleared out when starting a Real Human test run.

--- src/mail_panel/utils.py
+++ src/mail_panel/utils.py
@@ -13,3 +13,9 @@ def save_outbox(outbox):
     Saves the dictionary of cached mail and sets expiry.
     """
     cache.set(MAIL_TOOLBAR_CACHE_KEY, outbox, MAIL_TOOLBAR_TTL)
+    
+def clear_outbox():
+    """
+    Utility function to clear the dictionary of cached mail. Typical use case: Starting a new real-human test session.
+    """
+    cache.set(MAIL_TOOLBAR_CACHE_KEY, {})
